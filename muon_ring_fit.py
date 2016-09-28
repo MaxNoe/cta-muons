f8b8cc6f51b4f396c67674b24e61342d0980cea1
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-09-28 12:03:13 +0200

    Calculate completeness of rings

--- muon_ring_fit.py
+++ muon_ring_fit.py
@@ -1,6 +1,5 @@
 import argparse
 from astropy import log
-from ctapipe.utils.datasets import get_path
 from ctapipe.io.hessio import hessio_event_source
 import numpy as np
 from ctapipe.utils.linalg import rotation_matrix_2d
@@ -16,6 +15,7 @@ from ctapipe.calib.array.muon import (
     psf_likelihood_fit,
     mean_squared_error,
     photon_ratio_inside_ring,
+    ring_completeness,
 )
 
 
@@ -25,32 +25,35 @@ def rotate(x, y, angle):
 
 
 def fit_event(event, pixel_x, pixel_y, params):
+    pixel_x = pixel_x.value
+    pixel_y = pixel_y.value
+
+    result = {'event_number': event.count}
 
     event = calibrate_event(event, params)
     photons = event.dl1.tel[1].pe_charge
     time = event.dl1.tel[1].peakpos
-    size = np.sum(photons)
-
-    if size < 500:
-        return None
-
-    pixel_x = pixel_x.value
-    pixel_y = pixel_y.value
 
     mask = photons > 10
+    result['size'] = np.sum(photons[mask])
+    result['num_pixel'] = np.sum(mask)
 
-    if not np.any(mask):
+    if result['num_pixel'] < 5:
         return None
 
-    time_std = np.std(time[mask])
+    if result['size'] < 300:
+        return None
+
+    result['time_std'] = np.std(time[mask])
 
     r, x, y, sigma = psf_likelihood_fit(
         pixel_x[mask],
         pixel_y[mask],
         photons[mask],
     )
+    result.update(r=r, x=x, y=y, sigma=sigma)
 
-    ratio = photon_ratio_inside_ring(
+    result['ratio_inside'] = photon_ratio_inside_ring(
         pixel_x[mask],
         pixel_y[mask],
         photons[mask],
@@ -59,7 +62,8 @@ def fit_event(event, pixel_x, pixel_y, params):
         radius=r,
         width=sigma,
     )
-    error = mean_squared_error(
+
+    result['mean_squared_error'] = mean_squared_error(
         pixel_x[mask],
         pixel_y[mask],
         photons[mask],
@@ -68,17 +72,16 @@ def fit_event(event, pixel_x, pixel_y, params):
         radius=r,
     )
 
-    return {
-        'x': x,
-        'y': y,
-        'r': r,
-        'sigma': sigma,
-        'mean_squared_error': error,
-        'ratio_inside': ratio,
-        'event_number': event.count,
-        'size': size,
-        'time_std': time_std,
-    }
+    result['ring_completeness'] = ring_completeness(
+        pixel_x[mask],
+        pixel_y[mask],
+        photons[mask],
+        center_x=x,
+        center_y=y,
+        radius=r,
+    )
+
+    return result
 
 parser = argparse.ArgumentParser(description='Display each event in the file')
 parser.add_argument('inputfile')
